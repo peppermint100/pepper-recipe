2bf0fac8602254a846380cd531bd50e86f57f669
leeingyu <krpeppermint100@gmail.com>
2020-01-28 13:35:07 +0900

    Updates

---
+++ precache-manifest.538e7edb9c76ae79687bfe16fa23f2b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ce5c2873a5cc96eb3bac34f1dbe243",
+    "url": "/pepper-recipe/index.html"
+  },
+  {
+    "revision": "5b6a51ea8620946d2738",
+    "url": "/pepper-recipe/static/css/main.66688e40.chunk.css"
+  },
+  {
+    "revision": "60f90015a8dae4bd1b48",
+    "url": "/pepper-recipe/static/js/2.1c96e001.chunk.js"
+  },
+  {
+    "revision": "0ccca33dfb21399889824ced4d9b459c",
+    "url": "/pepper-recipe/static/js/2.1c96e001.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b6a51ea8620946d2738",
+    "url": "/pepper-recipe/static/js/main.31904a68.chunk.js"
+  },
+  {
+    "revision": "722b5d31457e23b93132",
+    "url": "/pepper-recipe/static/js/runtime-main.a2a2a043.js"
+  }
+]);
